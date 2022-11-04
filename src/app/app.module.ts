080370f70e956829f7ee8fcbf2fdbbf0f86958f4
DoEpgobXD <112198898+DoEpgobXD@users.noreply.github.com>
2022-11-04 13:58:04 -0500

    Friday 11/4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './Shared/header/header.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LandingPageComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
